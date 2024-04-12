a30391d37b948c5531874e05f856712c6a36d103
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-04-12 16:29:36 -0300

    Projeto finalizado

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { onMounted, reactive } from 'vue';
+import Message from './Message.vue';
 
 const obj = reactive({
     burgers: null,
     burger_id: null,
     status: [],
+    msg: null
 })
 
-const getPedidos = async () =>{
+const getPedidos = async () => {
     const req = await fetch('http://localhost:3000/burgers')
     const data = await req.json()
-    
+
     obj.burgers = data
 
     getStatus()
@@ -22,11 +24,47 @@ const getStatus = async () => {
 
     obj.status = data
 }
- 
+
+const deleteBurger = async (id) => {
+    const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "DELETE"
+    })
+
+    const res = await req.json()
+
+    obj.msg = `O pedido ${res.id} deletado com sucesso`
+
+    setTimeout(() => {
+        obj.msg = null
+    }, 5000);
+
+    getPedidos()
+}
+
+const updateBurger = async (event, id) => {
+    const option = event.target.value
+    const dataJson = JSON.stringify({ status: option })
+
+    const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson
+    })
+
+    const res = await req.json()
+    obj.msg = `O pedido ${res.id} foi atualizado para ${res.status}`
+
+    setTimeout(() => {
+        obj.msg = null
+    }, 5000);
+    
+}
+
 
 onMounted(getPedidos)
 </script>
 <template>
+    <Message :msg='obj.msg' v-show="obj.msg" />
     <table class="burger-table">
         <thead>
             <tr class="burger-table-heading">
@@ -50,18 +88,18 @@ onMounted(getPedidos)
                     </ul>
                 </td>
                 <td>
-                    <select name="status" class="status">
-                        <option value="">Selecionada</option>
-                        <option value="" v-for="status in obj.status" :key="status.id" :selected="burger.status == status.tipo">
+                    <select name="status" class="status" @change="updateBurger($event, burger.id)">
+                        <option :value="status.tipo" v-for="status in obj.status" :key="status.id"
+                            :selected="burger.status == status.tipo">
                             {{ status.tipo }}
                         </option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </td>
             </tr>
         </tbody>
     </table>
-    
+
 </template>
 
 <style scoped>
@@ -98,7 +136,7 @@ onMounted(getPedidos)
     width: 19%;
 }
 
-.burger__list{
+.burger__list {
     display: flex;
     flex-direction: column;
 }
